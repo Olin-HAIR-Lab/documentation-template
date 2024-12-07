9a8b411f5966ced38d37988556eb6c9c5cd96ca9
Dokyun Kim <123218910+dokyun-kim4@users.noreply.github.com>
2024-12-07 15:31:49 -0500

    documented python code example

---
+++ example.py
@@ -0,0 +1,34 @@
+"""
+Overview of what the code in this file does
+"""
+
+import numpy as np
+import os
+
+
+def do_very_hard_math(x: int, y:int) -> int: # Adding expected input & output type helps other people understand your code easily! For example, this function takes integers x & y and returns an integer.
+  """
+  Given two integers, this function returns the result of a very difficult math operation
+
+  Args:
+    x (int): first input
+    y (int): second input
+
+  Returns:
+    result (int): result of very hard math
+  """
+
+  # If you have many lines of code, it is good practice to comment sections of code for better readability
+
+  # Pretend like there was more code below here...
+
+  # Adds x and y
+  result = x + y
+
+
+  # Double the result
+  result *= 2
+  
+  return result
+  
+  
